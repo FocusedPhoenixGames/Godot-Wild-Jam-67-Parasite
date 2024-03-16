d08fcb8db594921c0c13daf655194ba870717efc
Alfrenzo <162761564+Alfrenzo@users.noreply.github.com>
2024-03-15 23:48:58 -0600

    Added back damage flash and made it so enemies keep attacking (in bit of a hacky way :P)

--- components/hurtbox_component.gd
+++ components/hurtbox_component.gd
@@ -3,10 +3,17 @@ class_name HurtboxComponent
 
 @export var healthComponent: Node
 @export var myHitboxComponent: HitboxComponent
+@export var sprite: Sprite2D
+
+@onready var whiteMaterialRes: ShaderMaterial = preload("res://assets/shaders/damage_flash_material.tres")
+var whitenMaterial: ShaderMaterial
+var shaderTween: Tween
 
 func _ready():
 	area_entered.connect(on_area_entered)
-
+	if sprite != null:
+		sprite.material = whiteMaterialRes.duplicate()
+		whitenMaterial = sprite.material as ShaderMaterial
 
 func on_area_entered(otherArea: Area2D):
 	if not otherArea is HitboxComponent:
@@ -20,4 +27,16 @@ func on_area_entered(otherArea: Area2D):
 		return
 	
 	healthComponent.damage(hitboxComponent.damage)
+	hitboxComponent.start_attack_cooldown()
+	damage_flash()
+
+func damage_flash():
+	if whitenMaterial == null:
+		return
 	
+	whitenMaterial.set_shader_parameter("whiten", 0.8)
+	
+	if shaderTween != null:
+		shaderTween.kill()
+	shaderTween = get_tree().create_tween()
+	shaderTween.tween_property(whitenMaterial, "shader_parameter/whiten", 0.0, 0.3)
