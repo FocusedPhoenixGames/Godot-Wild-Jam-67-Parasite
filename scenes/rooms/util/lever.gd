b1cd5b7d6a01903da0304767c924221dc9643c55
Alfrenzo <162761564+Alfrenzo@users.noreply.github.com>
2024-03-17 06:42:36 -0600

    Added sprite change and more util to lever

--- scenes/rooms/util/lever.gd
+++ scenes/rooms/util/lever.gd
@@ -1,23 +1,29 @@
 extends Area2D
 
 @export var pulled: bool = false
+@export var oneTimeOnly: bool = true
 @export var tile: int = 0
 @export var tilemap: TileMap
 
 @onready var button: Button = $Button
 @onready var sprite: Sprite2D = $Button/Sprite2D
+@onready var leverSprite: Sprite2D = $Sprite2D
 @onready var player = get_tree().root.get_node("Game").get_node("Player")
 @onready var doors = $"../Doors".get_children()
 
 var hovered = false
 var hasPlayer = false
+var usedOneTimeOnly = false
 
 func _ready():
 	self.body_entered.connect(_on_body_entered)
 	self.body_exited.connect(_on_body_entered)
 	button.mouse_entered.connect(on_enter_hover)
 	button.mouse_exited.connect(on_exit_hover)
-	lock_doors()
+	if !pulled:
+		lock_doors()
+	else:
+		leverSprite.frame = 1
 
 func _process(_delta):
 	handle_input()
@@ -44,8 +50,17 @@ func _on_body_entered(body):
 func handle_input():
 	if Input.is_action_just_pressed("interact"):
 		if sprite.visible:
-			pulled = true
-			open_doors()
+			if pulled and not usedOneTimeOnly:
+				pulled = false
+				leverSprite.frame = 0
+				lock_doors()
+			else:
+				pulled = true
+				leverSprite.frame = 1
+				open_doors()
+				
+				if oneTimeOnly:
+					usedOneTimeOnly = true
 
 func fill_collision_shape(shape: CollisionShape2D, erase: bool):
 	var rect = shape.shape.get_rect()
